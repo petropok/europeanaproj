9272ee7cf5f01c4955f8f4e26dc77006c90e8604
petropok <mimogios@gmail.com>
2022-03-23 03:16:55 +0200

    Progress on the API

--- routes.js
+++ routes.js
@@ -5,4 +5,17 @@ router.get('/', (req, res) => {
     res.render("pages/index");
 })
 
+router.get('/search', (req, res) => {
+    res.render("pages/search");
+})
+
+router.post('/', (req,res) => {
+    var query = req.body.search;
+    var htmlData = 'Hello:' + query;
+    if(query) {
+        res.render("pages/search", {term:query});
+    } else {
+        res.render("pages/index");
+    }
+ });
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const fetch = require('cross-fetch');
 const app = express()
 const PORT = 3000
 const router = require("./routes");
@@ -6,11 +7,48 @@ const router = require("./routes");
 app.use(express.static(__dirname + '/public'));
 
 app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+class SPARQLQueryDispatcher {
+	constructor( endpoint ) {
+		this.endpoint = endpoint;
+	}
+
+	query( sparqlQuery ) {
+		const fullUrl = this.endpoint + '?query=' + encodeURIComponent( sparqlQuery );
+		const headers = { 'Accept': 'application/sparql-results+json' };
+
+		return fetch( fullUrl, { headers } ).then( body => body.json() );
+	}
+}
+
+const endpointUrl = 'https://query.wikidata.org/sparql';
+const sparqlQuery = `#Get information of Europeana item using federated query
+PREFIX dc: <http://purl.org/dc/elements/1.1/>
+PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+PREFIX ore: <http://www.openarchives.org/ore/terms/>
+
+SELECT * WHERE {
+  BIND(<http://data.europeana.eu/proxy/provider/91622/raa_kmb_16000200042758> as ?p854)  
+  SERVICE <http://sparql.europeana.eu/> {
+   {
+         ?p854 <http://purl.org/dc/terms/created> ?created .
+         ?p854 <http://purl.org/dc/elements/1.1/identifier> ?identifier .
+         ?p854 <http://purl.org/dc/elements/1.1/publisher> ?publisher .
+         ?p854 <http://purl.org/dc/elements/1.1/rights> ?rights .
+         ?p854 <http://purl.org/dc/elements/1.1/title> ?title .
+         ?p854 <http://purl.org/dc/elements/1.1/description> ?description .
+     }
+  }
+}`;
+
 
 //Use external routes
 app.use("/", router);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
+    const queryDispatcher = new SPARQLQueryDispatcher( endpointUrl );
+    queryDispatcher.query( sparqlQuery ).then( console.log );
 });
